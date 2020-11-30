cef8874ddf8c2492e4e70be921294ca21fc496ac
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-11-30 12:18:32 +0100

    Fix SSR issue with react-aria

--- components/FormatPicker.js
+++ components/FormatPicker.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRadioGroupState } from "@react-stately/radio";
 import { useRadioGroup, useRadio } from "@react-aria/radio";
-
+import { SSRProvider } from "@react-aria/ssr";
 let RadioContext = React.createContext();
 function RadioGroup(props) {
   let { children, label } = props;
@@ -34,17 +34,19 @@ function Radio(props) {
 
 const FormatPicker = () => {
   return (
-    <RadioGroup className="flex flex-col items-start justify-start w-full overflow-hidden border border-blue-100 divide-y divide-blue-100 rounded-md">
-      <Radio defaultChecked description="With the .mp3 extension from the MPEG codec" value="mp3">
-        MP3 file format
-      </Radio>
-      <Radio description="With the .wav extension, made by IBM and Microsoft" value="wav">
-        WAV file format
-      </Radio>
-      <Radio description="With the .aac extension for Advanced Audio Coding" value="aac">
-        AAC file format
-      </Radio>
-    </RadioGroup>
+    <SSRProvider>
+      <RadioGroup className="flex flex-col items-start justify-start w-full overflow-hidden border border-blue-100 divide-y divide-blue-100 rounded-md">
+        <Radio defaultChecked description="With the .mp3 extension from the MPEG codec" value="mp3">
+          MP3 file format
+        </Radio>
+        <Radio description="With the .wav extension, made by IBM and Microsoft" value="wav">
+          WAV file format
+        </Radio>
+        <Radio description="With the .aac extension for Advanced Audio Coding" value="aac">
+          AAC file format
+        </Radio>
+      </RadioGroup>
+    </SSRProvider>
   );
 };
 
